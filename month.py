ed30a0ddd2bd3767331e480b9ff164c638464dc3
Hassanova123 <88289338+Hassanova123@users.noreply.github.com>
2021-09-12 00:44:21 +0100

    Initial commit

---
+++ month.py
@@ -0,0 +1,16 @@
+MonthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "APR": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(MonthConversions["Nov"])
